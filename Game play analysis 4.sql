6a4b9a2774696b6ef53ee75781723b0ac8bc2bba
Sai Keerthi <114061630+Keerthireddy585@users.noreply.github.com>
2025-12-13 20:32:46 +0530

    Game play analysis 4.sql

---
+++ Game play analysis 4.sql
@@ -0,0 +1,14 @@
+SELECT 
+    ROUND(
+        COUNT(a2.player_id) / COUNT(DISTINCT a1.player_id),
+        2
+    ) AS fraction
+FROM Activity a1
+LEFT JOIN Activity a2
+    ON a1.player_id = a2.player_id
+    AND DATE_ADD(a1.event_date, INTERVAL 1 DAY) = a2.event_date
+WHERE a1.event_date = (
+    SELECT MIN(event_date)
+    FROM Activity
+    WHERE player_id = a1.player_id
+);
