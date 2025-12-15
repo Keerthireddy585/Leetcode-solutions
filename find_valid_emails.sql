1abba875ee3f0e2238caa24428cefb5d4b58f468
Sai Keerthi <114061630+Keerthireddy585@users.noreply.github.com>
2025-12-15 14:22:01 +0530

    find_valid_emails.sql

--- find_valid_emails.sql
+++ find_valid_emails.sql
@@ -1,3 +1,6 @@
+-- LeetCode 3436: Find Valid E-mails
+-- Concept: REGEXP pattern Matching + string validation
+
 SELECT user_id, email
 FROM Users
 WHERE email REGEXP '^[A-Za-z0-9_]+@[A-Za-z]+\\.com$'
