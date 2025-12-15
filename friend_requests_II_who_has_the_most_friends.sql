3aa22bec062dd8642017934853ff47fea22fe0f4
Sai Keerthi <114061630+Keerthireddy585@users.noreply.github.com>
2025-12-15 14:24:28 +0530

    friend_requests_II_who_has_the_most_friends.sql

--- friend_requests_II_who_has_the_most_friends.sql
+++ friend_requests_II_who_has_the_most_friends.sql
@@ -1,3 +1,6 @@
+-- LeetCode 602: Friend Requests II - Who Has the Most Friends
+-- Concept: UNION ALL to combine requester & accepter + GROUP BY + COUNT  + ORDER BY + LIMIT
+
 SELECT id, COUNT(*) AS num
 FROM (
     SELECT requester_id AS id FROM RequestAccepted
