e4210fd5f7da6504271b29abfd25b92bdbfa7748
Sai Keerthi <114061630+Keerthireddy585@users.noreply.github.com>
2025-12-15 16:05:05 +0530

    primary_department _for_each_employee.sql

--- primary_department _for_each_employee.sql
+++ primary_department _for_each_employee.sql
@@ -1,3 +1,6 @@
+-- LeetCode 1789: Primary Department for Each Employee
+-- Concept: UNION + GROUP BY + HAVING + Conditional Filtering
+
 SELECT employee_id, department_id
 FROM Employee
 WHERE primary_flag = 'Y'
