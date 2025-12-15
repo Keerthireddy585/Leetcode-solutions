acbd21f642bd126fd9e65300d6301f467ebcbb1a
Sai Keerthi <114061630+Keerthireddy585@users.noreply.github.com>
2025-12-15 15:58:55 +0530

    not_boring_movies.sql

--- not_boring_movies.sql
+++ not_boring_movies.sql
@@ -1,3 +1,6 @@
+-- LeetCode 620: Not Boring Movies
+-- Concept: WHERE Filtering + Modulo Operator + ORDER BY
+
 SELECT id, movie, description, rating
 FROM Cinema
 WHERE id%2=1 AND description <> "boring"
