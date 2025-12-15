f74360ff45d91b20f33b0790df492d0c6210f445
Sai Keerthi <114061630+Keerthireddy585@users.noreply.github.com>
2025-12-15 13:56:50 +0530

    employee_bonus.sql

--- employee_bonus.sql
+++ employee_bonus.sql
@@ -1,4 +1,6 @@
-SELECT e.name, b.bonus
+-- LeetCode 577: Employee Bonus
+-- Concept: LEFT JOIN + NULL handlingSELECT e.name, b.bonus
+
 FROM Employee e
 LEFT JOIN Bonus b 
     ON e.empId = b.empId 
